d963d48fd46ec5b7641523463cc38029dfbcffad
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2022-05-09 11:45:49 -0300

    Add project files.

---
+++ CursoDesignPatterns_Alura/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns_Alura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
