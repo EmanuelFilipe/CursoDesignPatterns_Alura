8596cf3ed3712207143ea073720e803a3f6d3ed7
Emanuel Filipe <emanuel_filipe@yahoo.com.br>
2022-05-10 22:21:32 -0300

    template method

--- TemplateMethod/Item.cs
+++ TemplateMethod/Item.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CursoDesignPatterns_Alura
+namespace TemplateMethod
 {
     public class Item
     {
-        public string Nome { get; set; }
-        public double Valor { get; set; }
-
         public Item(string nome, double valor)
         {
             Nome = nome;
             Valor = valor;
         }
+
+        public string Nome { get; set; }
+        public double Valor { get; set; }
     }
 }
---
+++ TemplateMethod/Orcamento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    public class Orcamento
+    {
+        public Orcamento(double valor)
+        {
+            Valor = valor;
+            Itens = new List<Item>();
+        }
+
+        public double Valor { get; set; }
+        public IList<Item> Itens { get; set; }
+
+        public void AdicionaItem(Item item)
+        {
+            Itens.Add(item);
+        }
+    }
+}
